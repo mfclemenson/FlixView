94526bbccff3737c6886999509357c2c46fbe07a
mclem <mclemenson@gmail.com>
2018-08-12 23:24:00 -0400

    second

--- FlixView/App_Start/BundleConfig.cs
+++ FlixView/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace FlixView
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-yeti3.css",
                       "~/Content/site.css"));
         }
     }
