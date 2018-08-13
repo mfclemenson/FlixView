35c61014e8793baeafc4cf430803f47ba59c195b
mclem <mclemenson@gmail.com>
2018-08-12 23:23:25 -0400

    first commit

---
+++ FlixView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlixView.Startup))]
+namespace FlixView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
